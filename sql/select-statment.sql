c6ebdea320d3eaca7602fe643adf64292d59717c
Fatima Zahra <107687519+fatidevt@users.noreply.github.com>
2026-07-28 17:21:48 +0100

    Add files via upload

---
+++ sql/select-statment.sql
@@ -0,0 +1,15 @@
+SELECT 
+first_name,
+gender,
+age,
+age +10
+FROM employee_demographics;
+# SQL fllow this rule for math : PEMDAS
+#PEMDAS: Parentheses, Exponents, Multiplication and Division, and Addition and Subtraction
+
+
+select distinct
+first_name
+from employee_demographics;
+
+# distinct ==> only unique values
