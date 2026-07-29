a1911a33388549bd7ade9bec8cf8440a81866211
Fatima Zahra <107687519+fatidevt@users.noreply.github.com>
2026-07-29 21:09:59 +0100

    where and like statement
    Added multiple SELECT statements with various conditions and logical operators to the SQL file.

--- sql/select-statment.sql
+++ sql/select-statment.sql
@@ -1,3 +1,4 @@
+-- SELECT Statement
 SELECT 
 first_name,
 gender,
@@ -13,3 +14,38 @@ first_name
 from employee_demographics;
 
 # distinct ==> only unique values
+-- WHERE STETMENT
+
+SELECT * 
+FROM employee_demographics
+WHERE gender != 'Female'
+
+SELECT * 
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+
+SELECT * 
+FROM employee_salary
+WHERE salary >= 50000
+WHERE first_name = 'Lislie'
+
+-- Logical Operators : AND OR NOT --
+
+SELECT * 
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+OR NOT gender = 'Female';
+
+
+SELECT * 
+FROM employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44) or age > 55
+
+-- LIKE Statement
+--    %    ==> anything
+--    _    ==> spesific value
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'A__'
+WHERE first_name LIKE '%r%'
