ea9bc78bc94e836e41ed3c1c05488942cceb29f5
Fatima Zahra <107687519+fatidevt@users.noreply.github.com>
2026-09-04 23:12:39 +0100

    Add files via upload

---
+++ sql/section3/subq.sql
@@ -0,0 +1,37 @@
+select * from employee_salary;
+select * from employee_demographics;
+
+select * 
+from employee_demographics
+where employee_id in 
+	(select employee_id
+		from employee_salary
+        where dept_id = 1)
+;
+--
+select first_name, salary ,
+(select avg(salary) from employee_salary)
+from employee_salary;
+
+
+select gender, avg(age), max(age), min(age), count(age)
+from employee_demographics
+group by gender;
+
+
+select gender, avg(`max(age)`)
+from
+(select gender, avg(age), max(age), min(age), count(age)
+from employee_demographics
+group by gender) as agg_table
+group by gender;
+
+
+
+
+SELECT first_name, salary 
+FROM employee_salary
+WHERE salary > (
+    SELECT AVG(salary) 
+    FROM employee_salary
+);
